1996fd8a5b8121add222389814f11081c165663f
karl dudley <karldudley@gmail.com>
2022-10-11 15:41:25 +0100

    update scoreboard page

--- src/Pages/Scoreboard/index.js
+++ src/Pages/Scoreboard/index.js
@@ -1,66 +1,98 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Button, Grid, Divider } from "@mui/material";
 import { Link } from "react-router-dom";
+import axios from 'axios'
 import trophyIcon from './trophy-icon.png';
+import Dropdown from '../../components/Dropdown'
 import './style.css'
 
 const Scoreboard = () => {
+    const [data, setData] = useState([])
 
-    const data = [
-        {
-            name: 'Liam',
-            score: '8',
-            date: '08/10/2022'
-        },
-        {
-            name: 'Karl',
-            score: '10',
-            date: '09/10/2022'
-        },
-        {
-            name: 'Matt',
-            score: '5',
-            date: '01/10/2022'
-        },
-        {
-            name: 'Ayo',
-            score: '9',
-            date: '24/09/2022'
-        },
-        {
-            name: 'Mike',
-            score: '6',
-            date: '28/05/2022'
-        }
-    ]
+    const options = [
+        { label: 'All', value:""},
+        { label: 'General Knowledge', value: 'genKnow' },
+        { label: 'Books', value: 'books' },
+        { label: 'Film', value: 'film' },
+        { label: 'Music', value: 'music' },
+        { label: 'Musicals & Theatre', value: 'musicals' },
+        { label: 'Television', value: 'tv' },
+        { label: 'Video Games', value: 'vg' },
+        { label: 'Board Games', value: 'bg' },
+        { label: 'Science & Nature', value: 'science' },
+        { label: 'Computers', value: 'computers' },
+        { label: 'Mathematics', value: 'maths' }
+      ];
 
-    // useEffect(() => {
-    //     const fetchResult = async () => {
-    //         try {
-    //             // fetch from server
-    //         } catch (error) {
-    //             console.log(error)
-    //         }
+    const [category, setCategory] = useState("")
+
+    // const data = [
+    //     {
+    //         name: 'Liam',
+    //         score: '8',
+    //         date: '08/10/2022'
+    //     },
+    //     {
+    //         name: 'Karl',
+    //         score: '10',
+    //         date: '09/10/2022'
+    //     },
+    //     {
+    //         name: 'Matt',
+    //         score: '5',
+    //         date: '01/10/2022'
+    //     },
+    //     {
+    //         name: 'Ayo',
+    //         score: '9',
+    //         date: '24/09/2022'
+    //     },
+    //     {
+    //         name: 'Mike',
+    //         score: '6',
+    //         date: '28/05/2022'
     //     }
-    // })
-    const descending = data.sort((a,b) => b.score - a.score)
-    console.log(descending)
-    const renderResult = descending.map ((player) => {
+    // ]
+
+    useEffect(() => {
+        const fetchHighScore = async() => {
+          try {
+            const { data } = await axios.get(`https://quizzy-rascal-server.herokuapp.com/players/${category}`)
+            // const { data } = await axios.get(`http://localhost:8080/players/${category}`)
+            setData(data)
+          } catch (error) {
+            console.log(error)
+          } 
+        }
+    
+        fetchHighScore();
+        //only run useEffect when the category state changes
+    }, [category])
+
+    
+    const handleDropdown = (e) => {
+        setCategory(e.target.value)
+    }
+
+    const descending = data.sort((a,b) => b.highScore - a.highScore)
+    const renderResult = descending.slice(0, 10).map ((player) => {
+        let convertedDate = new Date(player.createdAt).toLocaleDateString('en-GB')
         return (
-            <>
+            <div key={player._id}>
             <Grid container className='grid' rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                 <Grid item xs={4} >
                     <div className="player-row">{player.name}</div>
                 </Grid>
                 <Grid item xs={4} >
-                    <div className="player-row">{player.score}</div>
+                    <div className="player-row">{player.highScore}</div>
                 </Grid>
                 <Grid item xs={4} >
-                    <div className="player-row">{player.date}</div>
+                    
+                    <div className="player-row">{convertedDate}</div>
                 </Grid>
-                </Grid>
+            </Grid>
             <Divider variant="middle" style={{background: 'white'}}/>
-            </>
+            </div>
         )
     })
 
@@ -77,6 +109,12 @@ const Scoreboard = () => {
     </div>
       <section className='score-board'>
         <h1 style={{textAlign: 'center', padding: '3rem 0rem'}}>Scoreboard</h1>
+        <Dropdown
+            label="Category: "
+            options={options}
+            value={category}
+            onChange={handleDropdown}
+        />
         <Grid container className='grid' rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
             <Grid item xs={4} >
                 <div className="header-row">Name</div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Nav } from './Nav'
 // export { default as JoinForm } from './JoinForm'
+export { default as Dropdown } from './Dropdown'
